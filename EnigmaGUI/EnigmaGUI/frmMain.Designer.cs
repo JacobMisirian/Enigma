9973632b7097f5b20265887e7600da7ab3e2ed26
Jacob Misirian <misirianrule@gmail.com>
2016-06-11 11:16:56 -0500

    Added GUI Implementation of lib

---
+++ EnigmaGUI/EnigmaGUI/frmMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace EnigmaGUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.nudSlow = new System.Windows.Forms.NumericUpDown();
+            this.nudMedium = new System.Windows.Forms.NumericUpDown();
+            this.nudFast = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSlow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMedium)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFast)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(84, 263);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(437, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(84, 147);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(437, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // nudSlow
+            // 
+            this.nudSlow.Location = new System.Drawing.Point(84, 54);
+            this.nudSlow.Name = "nudSlow";
+            this.nudSlow.Size = new System.Drawing.Size(62, 20);
+            this.nudSlow.TabIndex = 2;
+            // 
+            // nudMedium
+            // 
+            this.nudMedium.Location = new System.Drawing.Point(264, 54);
+            this.nudMedium.Name = "nudMedium";
+            this.nudMedium.Size = new System.Drawing.Size(61, 20);
+            this.nudMedium.TabIndex = 3;
+            // 
+            // nudFast
+            // 
+            this.nudFast.Location = new System.Drawing.Point(459, 54);
+            this.nudFast.Name = "nudFast";
+            this.nudFast.Size = new System.Drawing.Size(62, 20);
+            this.nudFast.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter input here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Output here:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Slow Rotor:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(264, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Medium Rotor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fast Rotor";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudFast);
+            this.Controls.Add(this.nudMedium);
+            this.Controls.Add(this.nudSlow);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmMain";
+            this.Text = "EngimaGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSlow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMedium)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFast)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.NumericUpDown nudSlow;
+        private System.Windows.Forms.NumericUpDown nudMedium;
+        private System.Windows.Forms.NumericUpDown nudFast;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EnigmaGUI/EnigmaGUI/frmMain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+using Enigma;
+
+namespace EnigmaGUI
+{
+    public partial class frmMain : Form
+    {
+        private EnigmaMachine machine;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            machine = new EnigmaMachine((uint)nudSlow.Value, (uint)nudMedium.Value, (uint)nudFast.Value);
+        }
+
+        private int lastLength = 0;
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            string text = txtInput.Text;
+            string selection = "";
+            bool append = false;
+            if (text.Length > lastLength)
+            {
+                selection = text.Substring(lastLength);
+                append = true;
+            }
+            else
+                selection = text;
+            lastLength = text.Length;
+
+            encode(selection, append);
+        }
+
+        private void encode(string text, bool append)
+        {
+            setRotors();
+            string result = machine.ProcessString(text);
+            if (append)
+                txtOutput.Text += result;
+            else
+                txtOutput.Text = result;
+            updateRotors();
+        }
+
+        private void updateRotors()
+        {
+            nudSlow.Value = (decimal)machine.SlowRotor;
+            nudMedium.Value = (decimal)machine.MediumRotor;
+            nudFast.Value = (decimal)machine.FastRotor;
+        }
+
+        private void setRotors()
+        {
+            machine.SlowRotor = (uint)nudSlow.Value;
+            machine.MediumRotor = (uint)nudMedium.Value;
+            machine.FastRotor = (uint)nudFast.Value;
+        }
+    }
+}
